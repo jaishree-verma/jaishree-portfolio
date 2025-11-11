7346f5e55765ecc4c485b5defe49d972c1b11949
jaishreeverma2004 <jaishree.verma.work@gmail.com>
2025-11-11 18:40:10 +0530

    Initial commit with updated portfolio and project data

---
+++ extra.js
@@ -0,0 +1,29 @@
+// Aesthetic Stopwatch
+  // {
+  //   id: 9,
+  //   title: 'Customizable, Aesthetic Stopwatch',
+  //   brief: 'My first ever JS Project: a web-based customizable stopwatch.',
+  //   tags: ['Web Dev', 'Design', 'Engineering'],
+  //   skills: ['HTML', 'CSS', 'Vanilla JavaScript', 'particles.js', 'FancyBox'],
+  //   thumbnail: '/aesthetic_stopwatch/1.png',
+  //   startDate: '2024-04',
+  //   endDate: '2024-05',
+  //   media: [
+  //     {
+  //       type: MEDIA_TYPES.IMAGE,
+  //       url: '/aesthetic_stopwatch/1.png',
+  //       caption: 'Aesthetic Stopwatch Interface on Laptop',
+  //       description: 'This stop watch project was my first ever web development project, and uses Vanilla J.S, & CSS.'
+  //     },
+  //     {
+  //       type: MEDIA_TYPES.IMAGE,
+  //       url: '/aesthetic_stopwatch/2.png',
+  //       caption: 'Stopwatch on Laptop Screen',
+  //       description: 'Image of the Aesthetic Stopwatch running on a an ipad..'
+  //     }
+  //   ],
+  //   // links: {
+  //   //   github: 'https://github.com/danielh-hong/browser_stopwatch',
+  //   //   live: 'https://aesthetic-stopwatch.netlify.app/'
+  //   // }
+  // },
